5611f1d424d9a3b6bef233f61043232b1b07c3e4
Evan Sparkman <me@evansparkman.com>
2019-03-24 23:26:11 -0400

    This is the beginning of something Amazing.

---
+++ gridsome.config.js
@@ -0,0 +1,12 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome with Netlify + NetlifyCMS + TailwindCSS',
+  siteDescription: 'Gridsome Starter Kit using Netlify for Deployments, NetlifyCMS for Content Management and TailwindCSS for Styling',
+  titleTemplate: 'Gridsome with Netlify + NetlifyCMS + TailwindCSS',
+  plugins: []
+}
